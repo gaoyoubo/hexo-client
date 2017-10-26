245c6361d3259ad6c7b0e39576a9ed74153c5454
GaoYoubo <gaoyoubo@foxmail.com>
2017-10-26 20:14:48 +0800

    记得先要解决布局样式问题。

--- src/renderer/components/Index.vue
+++ src/renderer/components/Index.vue
@@ -1,46 +1,23 @@
 <style scoped>
-  .layout {
-    border: 1px solid #d7dde4;
-    background: #f5f7f9;
-  }
-
-  .layout-content {
-    min-height: 200px;
-    margin: 0px;
-    overflow: hidden;
-    background: #fff;
-    border-radius: 4px;
-  }
-
-  .layout-copy {
-    text-align: center;
-    padding: 10px 0 20px;
-    color: #9ea7b4;
-  }
 </style>
 <template>
-  <div class="layout">
-    <div class="layout-content">
-      <Row>
-        <Col span="6" v-for="post in posts" style="padding: 10px;">
-          <Card>
-            <div slot="extra">
-              <Button type="primary" shape="circle" size="small" icon="edit"></Button>
-              <Button type="warning" shape="circle" size="small" icon="trash-a"></Button>
-            </div>
-            <div>
-              <p>{{ post.title }}</p>
-              <p>{{ post.author }}</p>
-              <p>{{ post.author }}</p>
-              <p><Tag v-for="tag in post.tags">{{ tag }}</Tag></p>
-            </div>
-          </Card>
-        </Col>
-      </Row>
-    </div>
-    <div class="layout-copy">
-      <a @click="copyright">http://www.mspring.org</a>
-    </div>
+  <div id="app">
+    <Row>
+      <Col span="6" v-for="post in posts" style="padding: 10px;">
+        <Card>
+          <div slot="extra">
+            <Button type="primary" shape="circle" size="small" icon="edit"></Button>
+            <Button type="warning" shape="circle" size="small" icon="trash-a"></Button>
+          </div>
+          <div>
+            <p>{{ post.title }}</p>
+            <p>{{ post.author }}</p>
+            <p>{{ post.author }}</p>
+            <p><Tag v-for="tag in post.tags">{{ tag }}</Tag></p>
+          </div>
+        </Card>
+      </Col>
+    </Row>
   </div>
 </template>
 
@@ -49,7 +26,13 @@
 
   export default {
     data () {
+      const item = {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }
       return {
+        tableData: Array(20).fill(item),
         posts: []
       }
     },
@@ -67,7 +50,6 @@
     methods: {
       copyright () {
         this.$store.commit('scanTags')
-        this.$electron.shell.openExternal('http://www.mspring.org')
       }
     },
     components: {}
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-import iView from 'iview'
-import 'iview/dist/styles/iview.css'
 import App from './App'
 import router from './router'
 import store from './store'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 
 // import axios from 'axios'
 // import 'bulma/css/bulma.css'
