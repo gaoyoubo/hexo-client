3c9a0ef905317b9c44187ee6138fb1898f0c4e77
GaoYoubo <gaoyoubo@foxmail.com>
2017-11-06 18:59:09 +0800

    加上数据库配置

--- src/renderer/components/Index.vue
+++ src/renderer/components/Index.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
-    <el-button type="primary" style="float: right; margin-right: 10px;">Git测试</el-button>
+    <el-button type="primary" style="float: right; margin-right: 10px;" @click="test">test</el-button>
     我是首页
   </div>
 </template>
 
 <script>
+  import Datastore from '../Datastore'
+  import {remote} from 'electron'
   export default {
     data () {
       return {}
     },
 
     methods: {
+      test () {
+        debugger
+        remote.app.getPath('userData')
+        Datastore.insert({'id': '1', 'name': '高尤波'}, function () {
+          debugger
+        })
+      }
     },
 
     beforeDestroy () {
