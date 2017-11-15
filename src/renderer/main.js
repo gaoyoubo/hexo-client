96de6dc02845d26b68e9de04927c971b82936be9
GaoYoubo <gaoyoubo@foxmail.com>
2017-11-15 20:12:17 +0800

    默认不打开控制台。

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -2,15 +2,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-
-// import axios from 'axios'
 
 if (!process.env.IS_WEB) {
   Vue.use(require('vue-electron'))
 }
-// Vue.http = Vue.prototype.$http = axios
+Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 
