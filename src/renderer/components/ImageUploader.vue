d56cda8444b32678e0a8784cf41a06eefe06f186
高尤波 <gaoyoubo@foxmail.com>
2017-12-02 15:08:15 +0800

    图片上床

---
+++ src/renderer/components/ImageUploader.vue
@@ -0,0 +1,74 @@
+<template>
+  <el-container>
+    <el-main>
+      <el-upload
+          action="https://jsonplaceholder.typicode.com/posts/"
+          list-type="picture-card"
+          accept="image/*"
+          :http-request="doUpload"
+          :multiple="true"
+          :on-preview="handlePictureCardPreview"
+          :on-remove="handleRemove">
+        <i class="el-icon-plus"></i>
+      </el-upload>
+      <el-dialog :visible.sync="dialogVisible" size="tiny">
+        <img width="100%" :src="dialogImageUrl" alt="">
+      </el-dialog>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+  import qiniu from 'qiniu'
+
+  export default {
+    data () {
+      return {
+        dialogImageUrl: '',
+        dialogVisible: false
+      }
+    },
+    methods: {
+      doUpload (arg) {
+        var config = new qiniu.conf.Config()
+        config.zone = qiniu.zone.Zone_z0
+
+        var accessKey = 'fFLsq2BPeDEXYsnkEpFlICPfsU6pYCfXV21GL8zY'
+        var secretKey = 'S3Qr4jS8WH5Fp-ubtVFelQnDk37M3j71q2Td5Xj2'
+        var mac = new qiniu.auth.digest.Mac(accessKey, secretKey)
+
+        var key = 'qiniu.mp4' // TODO 使用文件md5作为key
+        var options = {
+          scope: 'm-spring' + ':' + key
+        }
+        var putPolicy = new qiniu.rs.PutPolicy(options)
+        var uploadToken = putPolicy.uploadToken(mac)
+
+        var localFile = arg.file.path
+        var formUploader = new qiniu.form_up.FormUploader(config)
+        var putExtra = new qiniu.form_up.PutExtra()
+        // 文件上传
+        formUploader.putFile(uploadToken, key, localFile, putExtra,
+          function (respErr, respBody, respInfo) {
+            debugger
+            if (respErr) {
+              throw respErr
+            }
+            if (respInfo.statusCode === 200) {
+              console.log(respBody)
+            } else {
+              console.log(respInfo.statusCode)
+              console.log(respBody)
+            }
+          })
+      },
+      handleRemove (file, fileList) {
+        console.log(file, fileList)
+      },
+      handlePictureCardPreview (file) {
+        this.dialogImageUrl = file.url
+        this.dialogVisible = true
+      }
+    }
+  }
+</script>
