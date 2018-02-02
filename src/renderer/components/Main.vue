847b419e65f24adbb805fd19099c4ce55d976f9b
高尤波 <gaoyoubo@foxmail.com>
2018-02-02 19:20:12 +0800

    调整布局

--- src/renderer/components/Main.vue
+++ src/renderer/components/Main.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-container style="height: 500px; border: 1px solid #eee">
+  <el-container :style="{'height': windowHeight, 'border': 'solid 1px #eee;'}">
     <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
       <el-menu :default-openeds="['1', '3']">
         <el-submenu index="1">
@@ -88,6 +88,13 @@
   .el-aside {
     color: #333;
   }
+
+  html, body {
+    height: 100%;
+    overflow: hidden;
+    margin: 0px;
+    padding: 0px;
+  }
 </style>
 
 <script>
@@ -99,8 +106,24 @@
         address: '上海市普陀区金沙江路 1518 弄'
       }
       return {
+        windowHeight: '300px', // 窗口高度
         tableData: Array(20).fill(item)
       }
+    },
+
+    methods: {
+      handleResize () {
+        this.windowHeight = document.documentElement.clientHeight + 'px'
+      }
+    },
+
+    mounted () {
+      this.handleResize()
+      window.addEventListener('resize', this.handleResize)
+    },
+
+    beforeDestroy () {
+      window.removeEventListener('resize', this.handleResize)
     }
   }
 </script>
