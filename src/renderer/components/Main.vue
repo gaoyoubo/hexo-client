0ac68d95cf89afa08ce02846135667543ed2ca38
高尤波 <gaoyoubo@foxmail.com>
2018-02-07 11:55:56 +0800

    显示文章详情

--- src/renderer/components/Main.vue
+++ src/renderer/components/Main.vue
@@ -74,6 +74,7 @@
     overflow: hidden;
     margin: 0px;
     padding: 0px;
+    font-family: "Segoe UI", "Lucida Grande", Helvetica, Arial, "Microsoft YaHei", FreeSans, Arimo, "Droid Sans", "wenquanyi micro hei", "Hiragino Sans GB", "Hiragino Sans GB W3", FontAwesome, sans-serif;
   }
 
   .el-header {
