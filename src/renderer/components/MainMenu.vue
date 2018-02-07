a7563b3490d5128d200b9d2113f5d288bff00c5b
高尤波 <gaoyoubo@foxmail.com>
2018-02-07 10:04:44 +0800

    菜单默认激活状态可配置

--- src/renderer/components/MainMenu.vue
+++ src/renderer/components/MainMenu.vue
@@ -1,18 +1,23 @@
 <template>
-    <el-menu
-        default-active="posts"
-        class="el-menu-demo"
-        mode="horizontal"
-        background-color="#545c64"
-        text-color="#fff"
-        active-text-color="#ffd04b">
-      <el-menu-item index="posts">文章</el-menu-item>
-      <el-menu-item index="settings">配置</el-menu-item>
-    </el-menu>
+  <el-menu
+      :default-active="active"
+      mode="horizontal"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+    <el-menu-item index="posts">文章</el-menu-item>
+    <el-menu-item index="settings">配置</el-menu-item>
+  </el-menu>
 </template>
-\
+
 <script>
   export default {
+    props: {
+      active: {
+        type: String,
+        default: 'posts'
+      }
+    },
     data () {
       return {}
     }
