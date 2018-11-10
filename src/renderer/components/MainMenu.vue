578fdf08b6ebbf778248adf86258ac5b182d74b1
高尤波 <gaoyoubo@foxmail.com>
2018-11-10 14:03:49 +0800

    删掉导航组件注释掉的废弃代码

--- src/renderer/components/MainMenu.vue
+++ src/renderer/components/MainMenu.vue
@@ -6,8 +6,7 @@
              default-active="/main"
              background-color="#333"
              text-color="#fff"
-             active-text-color="#ffd04b"
-             class="el-menu-vertical-demo">
+             active-text-color="#ffd04b">
       <el-menu-item index="/main">
         <i class="el-icon-document"></i>
         <span slot="title">文章</span>
@@ -26,32 +25,6 @@
       </el-menu-item>
     </el-menu>
   </el-aside>
-  <!--
-  <el-menu
-      :router="true"
-      default-active="/posts"
-      mode="horizontal"
-      background-color="#545c64"
-      text-color="#fff"
-      active-text-color="#ffd04b">
-    <el-menu-item index="/posts">
-      <i class="el-icon-document"></i>
-      <span slot="title">文章</span>
-    </el-menu-item>
-    <el-menu-item index="/create">
-      <i class="el-icon-edit"></i>
-      <span slot="title">发表</span>
-    </el-menu-item>
-    <el-menu-item index="/settings">
-      <i class="el-icon-setting"></i>
-      <span slot="title">配置</span>
-    </el-menu-item>
-    <el-menu-item index="/about">
-      <i class="el-icon-info"></i>
-      <span slot="title">关于</span>
-    </el-menu-item>
-  </el-menu>
-  -->
 </template>
 
 <script>
@@ -68,11 +41,6 @@
     overflow: hidden;
     margin: 0px;
     padding: 0px;
-  }
-
-  .el-menu-vertical-demo:not(.el-menu--collapse) {
-    width: 200px;
-    min-height: 400px;
   }
 </style>
 
