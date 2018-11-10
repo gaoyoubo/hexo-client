66b34cfd99ffba0cb258df1b3ef7289bbd049d82
高尤波 <gaoyoubo@foxmail.com>
2018-11-10 16:31:31 +0800

    处理初始化的时候选择目录逻辑问题

--- src/renderer/store/modules/UiStatus.js
+++ src/renderer/store/modules/UiStatus.js
@@ -1,13 +1,16 @@
-// 记录页面滚动位置
-
+// 记录页面状态
 const state = {
   postListScrollTop: 0,
-  postListScrollLeft: 0
+  postListScrollLeft: 0,
+  dialogFormVisible: false
 }
 const mutations = {
   onscroll (state, obj) {
     state.postListScrollTop = obj.scrollTop
     state.postListScrollLeft = obj.scrollLeft
+  },
+  setDialogFormVisible (state, visible) {
+    state.dialogFormVisible = visible
   }
 }
 const actions = {
@@ -16,6 +19,9 @@ const actions = {
       scrollTop: obj.scrollTop,
       scrollLeft: obj.scrollLeft
     })
+  },
+  setDialogFormVisible (context, visible) {
+    context.commit('setDialogFormVisible', visible)
   }
 }
 export default {
