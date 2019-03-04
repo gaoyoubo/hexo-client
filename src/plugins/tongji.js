e0822897d496379f0e594845e9ee0168794e0e4d
高尤波 <gaoyoubo@foxmail.com>
2019-03-04 17:29:42 +0800

    .

--- src/plugins/tongji.js
+++ src/plugins/tongji.js
@@ -1,6 +1,6 @@
 import { ipcRenderer } from 'electron'
 import { ebtRenderer } from 'electron-baidu-tongji'
-import router from './router'
+import router from '@/router'
 
 // 百度统计
 const BAIDU_SITE_ID = '4b0947c1973db3139b436d8583b3fc00'
