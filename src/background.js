afeba5e57d86701893234f22786b8015fd50cdcd
高尤波 <gaoyoubo@foxmail.com>
2019-03-05 18:01:16 +0800

    mac下关闭按钮不直接退出应用

--- src/background.js
+++ src/background.js
@@ -48,9 +48,9 @@ app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
 
-  // if (process.platform !== 'darwin') {
-  app.quit()
-  // }
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
 
 app.on('activate', () => {
