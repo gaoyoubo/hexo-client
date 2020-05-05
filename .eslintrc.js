2bc00885ae56a6a65f499b26e5d207634fdc7d24
高尤波 <gaoyoubo@foxmail.com>
2020-05-05 10:35:12 +0800

    修改eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,8 +15,10 @@ module.exports = {
         args: "none"
       }
     ],
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    // "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    // "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-console": "off",
+    "no-debugger": "off",
     "vue/no-v-html": "off"
   }
 };
